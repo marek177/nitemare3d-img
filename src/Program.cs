57f7f7e6afff359004073f2befe96be9279eb14e
marek177 <156338151+marek177@users.noreply.github.com>
2026-09-19 07:54:15 +0200

    Add application entry point

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+namespace Nitemare3D.ImgEditor;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
